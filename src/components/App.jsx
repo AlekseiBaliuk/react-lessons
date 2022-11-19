0c329784fabd1831db3593ffbaeeb9d0a438949d
Aleksei Baliuk <aleksred.b@gmail.com>
2022-11-19 20:30:16 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,3 @@
-import { User } from './User/User';
-import { UsersList } from './UsersList/UsersList';
-import { Section } from './Section/Section';
-import { users } from 'data/users';
-import { GlobalStyles } from 'utils/GlobalStyle';
-
-const { name, email } = users[0];
-
 export const App = () => {
-  return (
-    <>
-      <Section>
-        <User name={name} email={email} />
-      </Section>
-      <Section title="List of users">
-        <UsersList users={users} />
-      </Section>
-      <GlobalStyles />
-    </>
-  );
+  return <div>React</div>;
 };
